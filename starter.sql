f49fa7038f61147a41873493079a62e24e94d63c
Allan Haggett <allan.haggett@gov.bc.ca>
2022-05-26 19:42:37 -0700

    Progress

--- starter.sql
+++ starter.sql
@@ -15,6 +15,24 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+
+DROP TABLE IF EXISTS `statuses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `statuses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `description` text,
+  `created` datetime NOT NULL,
+  `createdby` char(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `stat_createduser_ibfk_1` (`createdby`),
+  CONSTRAINT `stat_createduser_ibfk_1` FOREIGN KEY (`createdby`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=60022 DEFAULT CHARSET=latin1;
+
+
 --
 -- Table structure for table `courses`
 --
@@ -34,17 +52,13 @@ CREATE TABLE `courses` (
   `modified` datetime NOT NULL,
   `modifiedby_id` char(36) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `activities_ibfk_0` (`status_id`),
-  KEY `activityapprovedby_ibfk_1` (`approvedby_id`),
-  KEY `activitycreateduser_ibfk_1` (`createdby_id`),
-  KEY `activitymodifieduser_ibfk_1` (`modifiedby_id`),
-  CONSTRAINT `activities_ibfk_0` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`),
-  CONSTRAINT `activityapprovedby_ibfk_1` FOREIGN KEY (`approvedby_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `activitycreateduser_ibfk_1` FOREIGN KEY (`createdby_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `activitymodifieduser_ibfk_1` FOREIGN KEY (`modifiedby_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=161803399 DEFAULT CHARSET=latin1;
-
-
+  KEY `courses_ibfk_0` (`status_id`),
+  KEY `coursescreateduser_ibfk_1` (`createdby_id`),
+  KEY `coursesmodifieduser_ibfk_1` (`modifiedby_id`),
+  CONSTRAINT `courses_ibfk_0` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`),
+  CONSTRAINT `coursescreateduser_ibfk_1` FOREIGN KEY (`createdby_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `coursesmodifieduser_ibfk_1` FOREIGN KEY (`modifiedby_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16180339 DEFAULT CHARSET=latin1;
 
 
 --
@@ -62,20 +76,20 @@ CREATE TABLE `classes` (
   `slug` varchar(255) DEFAULT NULL,
   `hyperlink` varchar(255) DEFAULT NULL,
   `description` text,
+  `max_capacity` int(11) DEFAULT '100',
+  `min_capacity` int(11) DEFAULT '1',
   `status_id` int(11) DEFAULT '1',
   `created` datetime NOT NULL,
   `createdby_id` char(36) NOT NULL,
   `modified` datetime NOT NULL,
   `modifiedby_id` char(36) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `activities_ibfk_0` (`status_id`),
-  KEY `activityapprovedby_ibfk_1` (`approvedby_id`),
-  KEY `activitycreateduser_ibfk_1` (`createdby_id`),
-  KEY `activitymodifieduser_ibfk_1` (`modifiedby_id`),
-  CONSTRAINT `activities_ibfk_0` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`),
-  CONSTRAINT `activityapprovedby_ibfk_1` FOREIGN KEY (`approvedby_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `activitycreateduser_ibfk_1` FOREIGN KEY (`createdby_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `activitymodifieduser_ibfk_1` FOREIGN KEY (`modifiedby_id`) REFERENCES `users` (`id`)
+  KEY `classes_ibfk_0` (`status_id`),
+  KEY `classescreateduser_ibfk_1` (`createdby_id`),
+  KEY `classesmodifieduser_ibfk_1` (`modifiedby_id`),
+  CONSTRAINT `classes_ibfk_0` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`),
+  CONSTRAINT `classescreateduser_ibfk_1` FOREIGN KEY (`createdby_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `classesmodifieduser_ibfk_1` FOREIGN KEY (`modifiedby_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=314159 DEFAULT CHARSET=latin1;
 
 
@@ -85,14 +99,16 @@ CREATE TABLE `classes` (
 --
 
 DROP TABLE IF EXISTS `classes_courses`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `classes_courses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `course_id` int(11) NOT NULL,
   `class_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  KEY `classes_courses_ibfk_1` (`class_id`),
+  KEY `classes_courses_ibfk_2` (`course_id`),
+  CONSTRAINT `classes_courses_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`),
+  CONSTRAINT `classes_courses_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1000928 DEFAULT CHARSET=latin1;
 
 
 
@@ -114,17 +130,87 @@ CREATE TABLE `ministries` (
   `color` varchar(255) DEFAULT NULL,
   `featured` int(11) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=400200 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
+--
+-- Table structure for table `classes_ministries`
+--
+
+DROP TABLE IF EXISTS `classes_ministries`;
+CREATE TABLE `classes_ministries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `class_id` int(11) NOT NULL,
+  `ministry_id` int(11) NOT NULL,
+  `capacity` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `classes_ministries_ibfk_1` (`class_id`),
+  KEY `classes_ministries_ibfk_2` (`ministry_id`),
+  CONSTRAINT `classes_ministries_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`),
+  CONSTRAINT `classes_ministries_ibfk_2` FOREIGN KEY (`ministry_id`) REFERENCES `ministries` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1000928 DEFAULT CHARSET=latin1;
+
+--
+-- Table structure for table `sessions`
+--
+
+DROP TABLE IF EXISTS `sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `status_id` int(11) DEFAULT '1',
+  `class_id` int(11) DEFAULT '1',
+  `name` varchar(255) DEFAULT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `hyperlink` varchar(255) DEFAULT NULL,
+  `description` text,
+  `created` datetime NOT NULL,
+  `createdby_id` char(36) NOT NULL,
+  `modified` datetime NOT NULL,
+  `modifiedby_id` char(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sessions_ibfk_0` (`class_id`),
+  KEY `sessionstat_ibfk_0` (`status_id`),
+  KEY `sessionscreateduser_ibfk_1` (`createdby_id`),
+  KEY `sessionsmodifieduser_ibfk_1` (`modifiedby_id`),
+  CONSTRAINT `sessions_ibfk_0` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`),
+  CONSTRAINT `sessionstat_ibfk_0` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`),
+  CONSTRAINT `sessionscreateduser_ibfk_1` FOREIGN KEY (`createdby_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `sessionsmodifieduser_ibfk_1` FOREIGN KEY (`modifiedby_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=314159 DEFAULT CHARSET=latin1;
 
 
 
+--
+-- Table structure for table `registrants`
+--
 
-
-
-
+DROP TABLE IF EXISTS `registrants`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `registrants` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `employee_id` int(11) NOT NULL,
+  `status_id` int(11) DEFAULT '1',
+  `class_id` int(11) NOT NULL,
+  `ministry_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `hyperlink` varchar(255) DEFAULT NULL,
+  `created` datetime NOT NULL,
+  `createdby_id` char(36) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `registrants_ibfk_0` (`class_id`),
+  KEY `registrantstat_ibfk_0` (`status_id`),
+  KEY `registrant_ministry_ibfk_0` (`ministry_id`),
+  KEY `registrantscreateduser_ibfk_1` (`createdby_id`),
+  CONSTRAINT `registrants_ibfk_0` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`),
+  CONSTRAINT `registrant_ministry_ibfk_0` FOREIGN KEY (`ministry_id`) REFERENCES `ministries` (`id`),
+  CONSTRAINT `registrantsstat_ibfk_0` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`),
+) ENGINE=InnoDB AUTO_INCREMENT=314159 DEFAULT CHARSET=latin1;
 
 
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
